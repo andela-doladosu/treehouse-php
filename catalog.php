ef134b2516bd53f728cf2cc29bce9755167545e0
Dara Oladosu <daradosu@gmail.com>
2018-10-05 12:56:26 +0100

    get catalog categories from query string

--- catalog.php
+++ catalog.php
@@ -1,12 +1,21 @@
 <?php
 $pageTitle = "Full Catalog";
+
+if (isset($_GET['cat'])) {
+    if ($_GET['cat'] === 'books') {
+        $pageTitle = 'Books';
+    } elseif ($_GET['cat'] === 'movies') {
+        $pageTitle = 'Movies';
+    } elseif ($_GET['cat'] === 'music') {
+        $pageTitle = 'Music';
+    }
+}
+
 include("inc/header.php");
 ?>
 
 <div class="page section">
-    <h1>
-        Full Catalog
-    </h1>
+    <h1><?php echo $pageTitle; ?></h1>
 </div>
 
 <?php include("inc/footer.php"); ?>
