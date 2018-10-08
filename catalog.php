ab49ceeffa72622ddc0512c88227a782f1a3030d
Dara Oladosu <daradosu@gmail.com>
2018-10-08 08:08:40 +0100

    display catalog from data array

--- catalog.php
+++ catalog.php
@@ -1,4 +1,5 @@
 <?php
+include("inc/data.php");
 $pageTitle = "Full Catalog";
 $section = null;
 
@@ -18,8 +19,21 @@ if (isset($_GET['cat'])) {
 include("inc/header.php");
 ?>
 
-<div class="page section">
-    <h1><?php echo $pageTitle; ?></h1>
+<div class="page catalog section">
+    <div class="wrapper">
+        <h1><?php echo $pageTitle; ?></h1>
+        <ul class="items">
+            <?php
+            foreach ($catalog as $item) {
+                echo "<li><a href='#'><img src='"
+                    .$item['img']."' alt='"
+                    .$item['title']."' />"
+                    ."<p>view details</p>"
+                    ."</a></li>";
+            }
+            ?>
+        </ul>
+    </div>
 </div>
 
 <?php include("inc/footer.php"); ?>
