576d9bd49ee3016f15447d5f4450ca0e6caab198
Dara Oladosu <daradosu@gmail.com>
2018-10-11 11:40:19 +0100

    enable email sends

--- suggest.php
+++ suggest.php
@@ -1,4 +1,8 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+require_once 'vendor/PHPMailer-master/src/PHPMailer.php';
+require_once 'vendor/PHPMailer-master/src/Exception.php';
+require_once 'vendor/PHPMailer-master/src/SMTP.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = trim(filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING));
@@ -10,13 +14,56 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit;
     }
 
-    echo "<pre>";
+    if (!PHPMailer::validateAddress($email)) {
+        echo "invalid email address";
+        exit;
+    }
+
     $emailBody = "";
     $emailBody .= "Name: ". $name. "\n";
     $emailBody .= "Email: ". $email. "\n";
     $emailBody .= "Details: ". $details. "\n";
-    echo $emailBody;
-    echo "</pre>";
+
+    $mail = new PHPMailer;
+
+    //Tell PHPMailer to use SMTP
+    $mail->isSMTP();
+    //Enable SMTP debugging
+    // 0 = off (for production use)
+    // 1 = client messages
+    // 2 = client and server messages
+    $mail->SMTPDebug = 2;
+    //Set the hostname of the mail server
+    $mail->Host = 'smtp.gmail.com';
+    // use
+    // $mail->Host = gethostbyname('smtp.gmail.com');
+    // if your network does not support SMTP over IPv6
+    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+    $mail->Port = 587;
+    //Set the encryption system to use - ssl (deprecated) or tls
+    $mail->SMTPSecure = 'tls';
+    //Whether to use SMTP authentication
+    $mail->SMTPAuth = true;
+    //Username to use for SMTP authentication - use full email address for gmail
+    $mail->Username = "daradosu@gmail.com";
+    //Password to use for SMTP authentication
+    $mail->Password = "vgjounldthwafthf";
+
+    //It's important not to use the submitter's address as the from address as it's forgery,
+    //which will cause your messages to fail SPF checks.
+    //Use an address in your own domain as the from address, put the submitter's address in a reply-to
+    $mail->setFrom('daradosu@gmail.com', $name);
+    $mail->addAddress('daradosu@gmail.com', 'Dara Receiver');
+    $mail->addReplyTo($email, $name);
+    $mail->Subject = "Library Suggestion from $name";
+    $mail->Body = $emailBody;
+
+    if (!$mail->send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+
+        echo "<h1>mail send error</h1>";
+        exit;
+    }
 
     header("location:suggest.php?status=thanks");
 }
